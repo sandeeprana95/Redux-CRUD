200126583c6be8aa5622e17c816e7970a42065eb
sandeeprana95 <sandeeprana385@gmail.com>
2025-06-20 13:36:06 +0530

    create allProducts component

---
+++ src/components/AllProducts.jsx
@@ -0,0 +1,17 @@
+const AllProducts=()=>{
+    return(
+        <div className="h-screen flex items-center justify-center bg-gray-500">
+              <div className="bg-white p-2 grid grid-cols-3 gap-2 ">
+                 <div className="bg-gray-300 shadow-2xl rounded border p-2">
+                <h1>title</h1>
+                <p>price</p>
+                <p>discount</p>
+                <p>description</p>
+              </div>
+             
+            </div>
+        </div>
+    )
+}
+
+export default AllProducts
